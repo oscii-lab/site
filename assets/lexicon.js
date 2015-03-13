cbd40ed5131a74fb574322ca7cad4180499630c2
John DeNero <papajohn@gmail.com>
2015-03-13 08:36:52 -0700

    s/translation/translations and etc.

--- assets/lexicon.js
+++ assets/lexicon.js
@@ -1,4 +1,4 @@
-var endpoint = 'http://beta.lilt.com/translate/lexicon';
+var endpoint = 'http://localhost:8080/translate/lexicon';
 
 function searchLexicon(query) {
   var links = [];
@@ -9,11 +9,11 @@ function searchLexicon(query) {
         source: 'en',
         target: 'es'
       }, function(response) {
-        if (response.translation.length <= 0) return callback("No results");
-        links = _.map(response.translation, function(translation) {
-          return { source: query, score: translation.score*translation.score, target: translation.translation };
+        if (response.translations.length <= 0) return callback("No results");
+        links = _.map(response.translations, function(translation) {
+          return { source: query, score: translation.frequency, target: translation.target };
         });
-        callback(null, _.pluck(response.translation, 'translation'));
+        callback(null, _.pluck(response.translations, 'target'));
       });
     },
     function(queries, callback) {
@@ -23,10 +23,10 @@ function searchLexicon(query) {
           source: 'es',
           target: 'en'
         }, function(response) {
-          if (response.translation.length > 0) {
-            _.each(response.translation, function(translation) {
+          if (response.translations.length > 0) {
+            _.each(response.translations, function(translation) {
               if (translation.translation !== query) {
-                links.push({ target: q, score: translation.score*translation.score, source: translation.translation });
+                links.push({ target: q, score: translation.frequency, source: translation.target });
               }
             });
           }
--- pages/application.md
+++ pages/application.md
@@ -4,17 +4,17 @@
 </block styles>
 
 <block content>
-Current undergraduate and graduate Berkeley students are welcome to apply to
-join or start a research project. It is not possible for someone who is not
-affiliated with UC Berkeley to join. Please complete the following application.
+Current undergraduate students are welcome to apply to join or
+start a research project. It is not possible for someone who is not affiliated
+with UC Berkeley to join. Please complete the following application.
 
 Undergraduate requirements:
 
 * Completion and strong performance in CS 61A, CS 61B, and CS 70
 * Interest in natural language, visualization, and statistics
 
-All EECS graduate students are welcome to complete this application or just
-email John DeNero directly (denero@cs.berkeley.edu).
+All EECS graduate students should instead email John DeNero directly
+(denero@cs.berkeley.edu).
 
 <iframe src="https://docs.google.com/a/berkeley.edu/forms/d/1b8KfqS-H5DYeRkB78zYQtm10-h_UesJnzH13MBIoe2I/viewform?embedded=true"
 width="760" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
